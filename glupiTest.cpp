a7e7a1affdb4c89637a790254f1151e0d1dc5b4c
drazovko <drazovko@foi.hr>
2018-09-14 19:58:04 +0200

    Svašta napravljeno

--- glupiTest.cpp
+++ glupiTest.cpp
@@ -7,9 +7,97 @@
 #include<Poco/Net/SocketAddress.h>
 #include<Poco/Timespan.h>
 #include<Poco/Net/Socket.h>
+#include<thread>
+#include<mutex>
+#include<condition_variable>
+#include<array>
+#include<Poco/ByteOrder.h>
+
+template<typename Elem, size_t kapacitet>
+class KruzniSpremnik
+{
+private:
+    std::array<Elem, kapacitet> niz;
+    size_t pocetak;             //index početka cirkularnog spremnika
+    size_t broj;                //trenutni broj elemenata
+
+    std::mutex brava;
+    std::condition_variable daLiJePun;
+    std::condition_variable daLiJePrazan;
+
+public:
+    KruzniSpremnik() : pocetak{0}, broj{0} { }
+    
+    void Dodaj(const Elem& elem){
+        std::unique_lock<decltype(brava)> zasun(brava);
+        //stani ovdje ako više nema mjesta i čekaj obavijest:
+        daLiJePun.wait(zasun, [this] () { return broj != kapacitet; });
+        //dodaj element na kraj
+        niz[DajKraj()] = elem;
+        ++broj;
+
+        daLiJePrazan.notify_one();
+    }
+
+    Elem Sljedeci(){
+        std::unique_lock<decltype(brava)> zasun(brava);
+        daLiJePrazan.wait(zasun, [this](){ return broj != 0; });
+
+        //vadi element iz niza:
+        Elem rez = niz[pocetak];
+        pocetak = ++pocetak % kapacitet;
+        --broj;
+        //obavijesti nit koja čeka da je došlo do promjene:
+        daLiJePun.notify_one();
+        return rez;
+    }
+
+private:
+    //pomoćna funkcija za izračunavanje indeksa kraja niza
+    size_t DajKraj(){
+        return (pocetak + broj) % kapacitet;
+    }
+};
 
 using namespace std;
 using namespace Poco::Net;
+
+struct IdentifikacijaSocketa{
+    uint8_t tipArdese;
+    uint32_t IPAdresa;
+    uint16_t port;
+};
+
+struct PrijemnaPoruka{
+    uint8_t tipPoruke;
+    uint64_t identifikatorStrujanja;
+    IdentifikacijaSocketa adresa1;
+    IdentifikacijaSocketa adresa2;
+};
+
+KruzniSpremnik<PrijemnaPoruka, 100> cirkularniBafer; //gobalni spremnik
+Poco::ByteOrder byteOrderMoj;
+
+//dretva koja prazni spremnik
+void Trosilo(int id){
+    
+    while(true){
+        PrijemnaPoruka broj = cirkularniBafer.Sljedeci();
+        cout << "Trosilo " << id << " je dohvatilo " 
+             << hex << broj.identifikatorStrujanja << dec << endl;
+        
+    }
+    
+    
+}
+//dretva koja puni spremnik
+void Punjac(int n){
+    static uint64_t brojacPunjenja = 1;
+    
+    //cirkularniBafer.Dodaj(n);
+    cout << "Punjac je napravio " << brojacPunjenja++
+         << ".poruku" << endl;
+}
 
 enum konfParametri {
         IPadresa, port, relayServeri, nekiNoviParametar
@@ -49,7 +137,6 @@ private:
 public:
     UcitavanjeKonfiguracije() {
         ifstream tokPremaKonfiguraciji{imeUlazneKonfiguracije};
-        //tokPremaKonfiguraciji.getline(cijeliRed, 10000);
         while(getline(tokPremaKonfiguraciji, cijeliRed)){
             if ((!cijeliRed.empty()) && (cijeliRed != "\r")) {
                 sviParametri.push_back(cijeliRed);    
@@ -180,7 +267,6 @@ public:
                     }
                     popisAktivnihRelayPosluzitelja.append(socAddrRelayPosluzitelja.toString());
                     cout << "Potvrđen relay server: " << socAddrRelayPosluzitelja.toString() << endl;
-                    cout << stringZaPrijem << endl;
                 }
             }
             catch(const std::exception& e)
@@ -195,13 +281,15 @@ public:
     }
 };
 
+
+
 int main()
 {
-    const int vrijemeCekanjaUSecReceiveFrom = 3;
+    const int vrijemeCekanjaUSecReceiveFrom = 1;
     const int vrijemeCekanjaUMiliSecReceiveFrom = 0;
     Poco::Timespan timeSpanZaPrijem;
     timeSpanZaPrijem.assign(vrijemeCekanjaUSecReceiveFrom, vrijemeCekanjaUMiliSecReceiveFrom);
-    char buffer[1024];
+    u_char poljeZaPrijem[1024];
 
     //1. FAZA INICIJALIZACIJE
         //učitavanje parametara iz konfiguracijske datoteke u objekt citac
@@ -220,9 +308,57 @@ int main()
     provjeraRelayPosluzitelja.Provjera(ds);
     cout << "\nPopis aktivnih relay posluzitelja: " << provjeraRelayPosluzitelja.DajPopisAktivnihPosluzitelja() << endl;
     //2. FAZA RADNOG REŽIMA
-    
+    timeSpanZaPrijem.assign(0, 0);
+    ds.setReceiveTimeout(timeSpanZaPrijem);
     SocketAddress posiljatelj;
+    cout << "Server je na prijemu . . ." << endl << endl;
     
+    
+    thread t1{Trosilo, 1};
+    thread t2{Trosilo, 2};
+   
+    uint64_t brojacPunjenja = 1;
+    PrijemnaPoruka prijemnaPoruka;
+    PrijemnaPoruka* pokPrijemnaPoruka;
+
+    while(1){
+        ds.receiveFrom(poljeZaPrijem, sizeof(poljeZaPrijem), posiljatelj);
+        cout << "Posiljatelj: " << posiljatelj.toString() << endl;
+        
+        cout << "Hex: " << endl;
+        for(int i = 1; i<=30; i++){
+            cout << i << " ";
+        }
+        int BB;
+        cout << endl << hex;
+        for(int i = 0; i<30; i++){
+            BB = (int)poljeZaPrijem[i];
+            cout << BB << " ";
+        }
+        cout << endl << dec; 
+        
+        pokPrijemnaPoruka = (PrijemnaPoruka*)&poljeZaPrijem[0];
+        prijemnaPoruka = *pokPrijemnaPoruka;
+        cout << "Punjac je napravio " << brojacPunjenja++
+              << ".poruku, " << hex
+              << prijemnaPoruka.identifikatorStrujanja << dec << endl;
+        cirkularniBafer.Dodaj(prijemnaPoruka);
+        
+
+        
+    }
+
+    t1.join();
+    t2.join();
+    
+
+
+
+    //testiranje
+    SocketAddress saTest("192.168.005.104", 11000);
+    char buffer[1024];
+
+    ds.sendTo(buffer, 1024, saTest);
     try
     {
         ds.receiveFrom(buffer, sizeof(buffer), posiljatelj);
